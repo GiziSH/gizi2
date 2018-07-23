c83b9ea5d9d4e6d032e102ca3532105f47de3e40
suhyun <ho03113@gmail.com>
2018-07-24 00:11:02 +0900

    검색-리스트뷰클릭-옵션화면-화장실상태 - 최근검색(상태바로보기설정x)

--- app/src/main/java/com/suhyun/gizi2/Fragment2.java
+++ app/src/main/java/com/suhyun/gizi2/Fragment2.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,6 +15,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,12 +29,14 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class Fragment2 extends Fragment {
-
     Button mBtn;
+    Button deleteBtn;
     private List<String> list_names;
     private SharedPreferences pref;
     private SharedPreferences.Editor editor;
     private ListView rs_listview;
+    private ArrayAdapter rsadapter2;
+
     public Fragment2() {
         // Required empty public constructor
     }
@@ -46,12 +51,14 @@ public class Fragment2 extends Fragment {
         editor = pref.edit();
 
         rs_listview = (ListView) v.findViewById(R.id.recent_listview);
+        TextView tx = (TextView)v.findViewById(R.id.empty_text);
+        rs_listview.setEmptyView(tx);
+
 
         list_names = new ArrayList<>();
         showLately();
-        ArrayAdapter<String> rsadapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_multiple_choice,list_names);
+        final ArrayAdapter<String> rsadapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,list_names);
         rs_listview.setAdapter(rsadapter);
-
 
 
         mBtn = (Button)v.findViewById(R.id.popup_menu);
@@ -59,12 +66,11 @@ public class Fragment2 extends Fragment {
 
             @Override
             public void onClick(View v) {
-                PopupMenu popup = new PopupMenu(getContext(),v); //팝업메뉴 객체만듦
 
+                final PopupMenu popup = new PopupMenu(getContext(),v); //팝업메뉴 객체만듦
                 //xml파일에 메뉴 정의한 것 가져오기  위한 전개자 선언
                 MenuInflater inflater = popup.getMenuInflater();
                 Menu menu = popup.getMenu();
-
                 //실제 메뉴 정의한 것 가져오는 부분 menu객체 넣어주기
                 inflater.inflate(R.menu.popupmenu, menu);
 
@@ -74,10 +80,38 @@ public class Fragment2 extends Fragment {
                     public boolean onMenuItemClick(MenuItem item) {
                         switch(item.getItemId()){
                             case R.id.popup_select:
-                                mBtn.setBackgroundColor(Color.RED);
+                                ArrayAdapter<String> rsadapter2 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_multiple_choice,list_names);
+                                rs_listview.setAdapter(rsadapter2);
+                                rs_listview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                                popup.dismiss();
                                 break;
                             case R.id.popup_delete:
-                                mBtn.setBackgroundColor(Color.BLUE);
+                                SparseBooleanArray checkedItems = rs_listview.getCheckedItemPositions();
+                                int count = rsadapter.getCount() ;
+                                for (int i = count-1; i >= 0; i--) {
+                                    if (checkedItems.get(i)) {
+                                        list_names.remove(i) ;
+                                    }
+                                }
+                                // 모든 선택 상태 초기화.
+                                rs_listview.clearChoices() ;
+                                saveLately();
+                                rsadapter.notifyDataSetChanged();
+                                rs_listview.setAdapter(rsadapter);
+                                break;
+                            case R.id.popup_allselect:
+                                rsadapter2 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_multiple_choice,list_names);
+                                rs_listview.setAdapter(rsadapter2);
+                                rs_listview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                                popup.dismiss();
+
+                                count = rsadapter.getCount() ;
+
+                                for (int i=0; i<count; i++) {
+                                    rs_listview.setItemChecked(i, true) ;
+                                }
+                                popup.dismiss();
+
                                 break;
 
                         }
@@ -87,8 +121,21 @@ public class Fragment2 extends Fragment {
                 popup.show();
             }
         });
+
         return v;
     }
+    //내부메모리에 저장
+    public void saveLately(){
+        JSONArray array = new JSONArray();
+        for(int i=0; i<list_names.size();i++){
+            array.put(list_names.get(i));
+        }
+        String a = array.toString();
+
+        editor.putString("lately", a);
+        editor.commit();
+    }
+
     //내부메모리에서 불러오기
     public void showLately(){
         String json = pref.getString("lately", null);

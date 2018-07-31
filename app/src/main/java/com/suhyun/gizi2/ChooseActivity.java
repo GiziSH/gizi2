f22c1b3089628b5dccafd725941fcf38ee42d75a
suhyun <ho03113@gmail.com>
2018-07-31 15:15:25 +0900

    -3-

--- app/src/main/java/com/suhyun/gizi2/ChooseActivity.java
+++ app/src/main/java/com/suhyun/gizi2/ChooseActivity.java
@@ -25,5 +25,14 @@ public class ChooseActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ImageButton manager = (ImageButton)findViewById(R.id.manager);
+        manager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//버튼 눌렸을 때
+                Intent intent = new Intent(ChooseActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/suhyun/gizi2/signin.java
@@ -0,0 +1,16 @@
+package com.suhyun.gizi2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by suhyun on 2018-07-31.
+ */
+
+public class signin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+    }
+}
